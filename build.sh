dba0e255a93e7cd7ffe744e9b838b41270e45f1f
Derkai <2363104914@qq.com>
2024-08-20 15:58:40 +0800

    Doc: update README.

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-
 set -e
 cd "$(dirname "$0")"
 
+# 下载依赖项
 sudo apt-get install libcurl4-openssl-dev
 sudo apt install libcjson1 libcjson-dev
 
-# Install Conan packages
+# 构建并编译
 mkdir -p build
 cd build
 cmake ..
